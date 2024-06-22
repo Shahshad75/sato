739d6c75d831e6298f775ca823d78c5d813ffcc2
shahshad <www.shashadbabu.com@gmail.com>
2024-06-22 14:49:17 +0530

    upadate change

--- lib/controller/home_controller.dart
+++ lib/controller/home_controller.dart
@@ -1,9 +1,13 @@
+// // ignore_for_file: depend_on_referenced_packages
+
 // import 'dart:io';
 
 // import 'package:flutter/services.dart';
 // import 'package:get/get.dart';
 // import 'package:image_picker/image_picker.dart';
 // import 'package:shared_preferences/shared_preferences.dart';
+// import 'package:path_provider/path_provider.dart';
+// import 'package:path/path.dart' as path;
 
 // class HomeController extends GetxController {
 //   File? image;
@@ -35,15 +39,36 @@
 //     try {
 //       final String? imagePath = await platform.invokeMethod('getSharedImage');
 //       if (imagePath != null) {
-//         image = File(imagePath);
-//         print("============image=======$image==");
-//         update();
+//         File? sharedImageFile = await _getFileFromUri(imagePath);
+//         if (sharedImageFile != null) {
+//           image = sharedImageFile;
+//           print("============image=======$image==");
+//           update();
+//         }
 //       }
 //     } on PlatformException catch (e) {
 //       print("Failed to get shared image: '${e.message}'.");
 //     }
 //   }
-// }
+
+//   Future<File?> _getFileFromUri(String uriString) async {
+//     try {
+//       final Uri uri = Uri.parse(uriString);
+//       if (uri.scheme == 'content') {
+//         final Directory tempDir = await getTemporaryDirectory();
+//         final String tempPath = path.join(tempDir.path, 'shared_image.jpg');
+//         final File tempFile = File(tempPath);
+//         final byteData = await platform.invokeMethod('readBytes', uriString);
+//         await tempFile.writeAsBytes(byteData);
+//         return tempFile;
+//       } else {
+//         return File(uriString);
+//       }
+//     } catch (e) {
+//       print("Error while converting URI to file: $e");
+//       return null;
+//     }
+//   }
 
 import 'dart:io';
 
@@ -59,6 +84,18 @@ class HomeController extends GetxController {
 
   static const platform = MethodChannel('imageUploader/sharedImage');
 
+  @override
+  void onInit() {
+    super.onInit();
+    loadImage();
+    handleSharedImage();
+    platform.setMethodCallHandler((call) async {
+      if (call.method == "newIntent") {
+        await handleSharedImage();
+      }
+    });
+  }
+
   Future<void> loadImage() async {
     final prefs = await SharedPreferences.getInstance();
     final imagePath = prefs.getString('selected_image_path');
@@ -69,7 +106,7 @@ class HomeController extends GetxController {
     }
   }
 
-  imagepicked(source) async {
+  Future<void> imagepicked(ImageSource source) async {
     final picker = ImagePicker();
     final pickedfile = await picker.pickImage(source: source);
     if (pickedfile != null) {
@@ -87,7 +124,7 @@ class HomeController extends GetxController {
         File? sharedImageFile = await _getFileFromUri(imagePath);
         if (sharedImageFile != null) {
           image = sharedImageFile;
-          print("============image=======$image==");
+          print("yes its down");
           update();
         }
       }
